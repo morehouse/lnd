dbe0360b26861f9923ac29a454d7337bc3cc779d
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-07-27 10:04:32 -0400

    itest: rename basic forward interceptor
    As is, forward interceptor and forward interceptor dedup htlcs will
    both be run when you specify icase=forward_interceptor. Update test
    name to be able to run in isolation.

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -427,7 +427,7 @@ var allTestCases = []*lntest.TestCase{
 		TestFunc: testForwardInterceptorDedupHtlc,
 	},
 	{
-		Name:     "forward interceptor",
+		Name:     "basic forward interceptor",
 		TestFunc: testForwardInterceptorBasic,
 	},
 	{
